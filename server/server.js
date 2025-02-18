6326db2539a802de14af3dec11db65b45dca5a58
Rohith-S-07 <rohithskr01@gmail.com>
2025-02-18 14:04:56 +0530

    Changes on Backend to Host

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db'); // Import the connectDB function
+const connectDB = require('./config/db');
 const fs = require('fs');
 const path = require('path');
 const app = express();
@@ -16,20 +16,10 @@ dotenv.config();
 
 connectDB();
 
+app.use(cors());
+app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Configure CORS
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true
-};
-
-
-
-app.use(cors(corsOptions));
-app.use(express.json());
-
-// Your routes
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const agencyRoutes = require('./routes/agencyRoutes');
@@ -37,7 +27,6 @@ const sosRoutes = require('./routes/sosRoutes');
 const resourceRoutes = require('./routes/resourcesRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const chatroomRoutes = require('./routes/chatroomRoutes');
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
@@ -47,9 +36,6 @@ app.use('/api/resources', resourceRoutes);
 app.use('/api/contactus', contactRoutes);
 app.use('/api/admin/contactus', contactRoutes);
 app.use('/api/chatrooms', chatroomRoutes);
-
-
-
 
 const PORT = process.env.PORT || 5000;
 
